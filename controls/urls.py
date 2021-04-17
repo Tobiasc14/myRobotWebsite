31dd857b88bbe69badae23c550c59e522c4bf4d6
Tobias <spamalot7200@gmail.com>
2021-04-17 11:05:40 -0400

    added basic framework of website with contols, are you ready, and ready pages

--- controls/urls.py
+++ controls/urls.py
@@ -4,6 +4,8 @@ from . import views
 # After website urls (robotWebsite\urls) sends here, it looks to see what urls are associated
 # with controls. What to do with a given URL is defined in the views page
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('test/', views.test, name = 'test')
+    path('', views.controls, name='Controls'),
+    path('test/', views.test, name = 'test'),
+    path('areyouready/', views.areYouReady, name = 'Ready to Go?'),
+    path('ready', views.driving, name = 'Driving')
 ]
--- controls/views.py
+++ controls/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 #This is a view called index
-def index(request):
-    return HttpResponse("Hello, world. You're at the controls index.")
+def controls(request):
+    return HttpResponse("This is the controls page.")
 
 def test(request):
     return HttpResponse("Test was sucessful")
+
+def areYouReady(request):
+    return HttpResponse('Are you Ready to Drive?')
+
+def driving(request):
+    return HttpResponse('Driving')
