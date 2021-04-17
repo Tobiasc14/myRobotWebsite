e95fa15ce57a2d3f864dc2a121bc2c6d2128e6a3
Tobias <spamalot7200@gmail.com>
2021-04-17 18:37:10 -0400

    Link isn't quite working, but what I have looks nice

--- controls/urls.py
+++ controls/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 # After website urls (robotWebsite\urls) sends here, it looks to see what urls are associated
 # with controls. What to do with a given URL is defined in the views page
+app_name = 'controls'
 urlpatterns = [
     path('', views.controls, name='Controls'),
     path('test/', views.test, name = 'test'),
