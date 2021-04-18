0d1bd489d42e5455bc4715d2a0158e3be21297ef
Tobias <spamalot7200@gmail.com>
2021-04-18 09:42:25 -0400

    Began designing actual ready driving page, implemented python code to automatically put in any buttons I want

--- controls/urls.py
+++ controls/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
 
+
 # After website urls (robotWebsite\urls) sends here, it looks to see what urls are associated
 # with controls. What to do with a given URL is defined in the views page
 app_name = 'controls'
 urlpatterns = [
     path('', views.controls, name='Controls'),
     path('test/', views.test, name = 'test'),
-    path('areyouready/', views.areYouReady, name = 'Are You Ready?'),
     path('ready/', views.driving, name = 'Driving')
 ]
--- controls/views.py
+++ controls/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, get_list_or_404
 from django.http import HttpResponse
+from controls.models import Button
 
 #This is a view called index
+# Third variable of each render request is a context, mapping a term in the template
+# folder back to
 def controls(request):
     return render(request, 'controls/controls.html')
 
 def test(request):
     return HttpResponse("Test was sucessful")
 
-def areYouReady(request):
-    return render(request, 'controls/areyouready.html')
-
 def driving(request):
-    return HttpResponse('Driving')
+    button = get_list_or_404(Button.objects.all())
+    return render(request, 'controls/ready.html', {'button': button})
