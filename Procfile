543fc8074a063d0602811994940031f6c0a8f749
Tobias <spamalot7200@gmail.com>
2021-04-28 21:53:49 -0400

    I think I added all the heroku stuff?

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python myRobotWebsite/manage.py migrate
+web: cd myRobotWebsite; gunicorn myRobotWebsite.wsgi
--- robotWebsite/settings.py
+++ robotWebsite/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,10 +120,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
