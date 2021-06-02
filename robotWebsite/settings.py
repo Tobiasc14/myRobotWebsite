9e9e6d4a11c9148fec5b5a3ed4fa279d00dbec2b
Tobias <spamalot7200@gmail.com>
2021-06-02 19:08:26 -0400

    added line from heroku page about static files

--- robotWebsite/settings.py
+++ robotWebsite/settings.py
@@ -127,7 +127,9 @@ STATICFILES_DIRS = [
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
