5dc46675dcc6992bc38f4fee76906a3c1c520fad
Tobias <spamalot7200@gmail.com>
2021-06-02 18:25:18 -0400

    Got static images working hopefully

--- controls/urls.py
+++ controls/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 # After website urls (robotWebsite\urls) sends here, it looks to see what urls are associated
@@ -11,4 +13,4 @@ urlpatterns = [
     path('ready/', views.ready, name = 'ready'),
     path('secret/', views.secret, name = 'secret'),
     path('driving/', views.driving, name = 'driving'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
