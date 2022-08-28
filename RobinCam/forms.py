d96f7df5c035fa47b0fa7a5847fa890f12cbe338
Tobiasc14 <Spamalot7200@gmail.com>
2022-08-28 10:34:24 -0400

    Added app label to metadata in form class, maybe now it knows what app its for?

--- RobinCam/forms.py
+++ RobinCam/forms.py
@@ -5,5 +5,6 @@ from .models import Picture
 class ImageForm(forms.ModelForm):
     """Form for the image model"""
     class Meta:
+        app_label = 'RobinCam'
         model = Picture
         fields = ('title', 'picture')
