1aa0587ec5c438a28788b9de064a162e217c8dea
Tobiasc14 <Spamalot7200@gmail.com>
2022-08-28 10:37:25 -0400

    Added app label to metadata in models class, maybe now it knows what app its for?

--- RobinCam/models.py
+++ RobinCam/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Picture(models.Model):
+    app_label = 'RobinCam'
     title = models.CharField(max_length = 30, default="Robin")
     picture = models.ImageField(upload_to='media/RobinCam/images')
 
