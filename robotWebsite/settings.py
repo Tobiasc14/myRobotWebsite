a60f4c09dabc091173e9be3b552f7fe71c25235b
Tobiasc14 <Spamalot7200@gmail.com>
2022-08-28 08:51:43 -0400

    Removed the commend from media_url so now it can look there

--- robotWebsite/settings.py
+++ robotWebsite/settings.py
@@ -133,7 +133,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
 
-#MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
  # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
