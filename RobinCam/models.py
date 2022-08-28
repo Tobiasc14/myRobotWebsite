c943e4ab08b72ee293b1ff179590c6649f6f3bd5
Tobiasc14 <Spamalot7200@gmail.com>
2022-08-28 08:12:13 -0400

    Fixed something I think?

--- RobinCam/models.py
+++ RobinCam/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Picture(models.Model):
-
-    image = models.ImageField(upload_to='staticfiles/images')
+    name = models.CharField(max_length = 30, default="Robin")
+    image = models.ImageField(upload_to='RobinCam/images')
 
 
