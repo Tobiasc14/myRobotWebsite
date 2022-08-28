34bb76c100481c828b0c0c8a2a3dda13ebec24e2
Tobiasc14 <Spamalot7200@gmail.com>
2022-08-28 08:47:47 -0400

    Added form so you can upload an image to the website, left debut=True to see if heroku will serve the image if that setting is on

--- RobinCam/models.py
+++ RobinCam/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Picture(models.Model):
-    name = models.CharField(max_length = 30, default="Robin")
-    image = models.ImageField(upload_to='RobinCam/images')
+    title = models.CharField(max_length = 30, default="Robin")
+    picture = models.ImageField(upload_to='RobinCam/images')
 
 
--- RobinCam/views.py
+++ RobinCam/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 
 # Create your views here.
+from RobinCam.forms import ImageForm
 from RobinCam.models import Picture
 
 
 def robinCam(request):
-    picture = get_object_or_404(Picture)
-    url = picture.image.url
-    return render(request, 'RobinCam/whatthedogdoin.html', {'location': url})
+    #picture = get_object_or_404(Picture)
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # Get the current instance object to display in the template
+            img_obj = form.instance
+            loc = img_obj.picture.url
+            title = img_obj.title
+            return render(request, 'RobinCam/whatthedogdoin.html', {'form': form, 'img_obj': loc, 'title': title})
+    else:
+        form = ImageForm()
+    return render(request, 'RobinCam/whatthedogdoin.html', {'form': form})
+
+
+    #return render(request, 'RobinCam/whatthedogdoin.html', {'image': picture})
