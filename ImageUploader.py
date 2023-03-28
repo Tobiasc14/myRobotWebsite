a9c52915d0f76a1298cceabd209314d455b36177
Tobiasc14 <Spamalot7200@gmail.com>
2023-03-27 22:56:30 -0400

    Added code that lets you automatically upload an image to the webpage

---
+++ ImageUploader.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+import time
+
+# Set up the webdriver
+browser = webdriver.Chrome()
+
+# Go to the website URL
+url = "http://127.0.0.1:8000/robincam/"
+browser.get(url)
+
+# Find the file input element and send the image file path to it
+file_input = browser.find_element(by='name', value='picture')
+file_path = "C:/Users/hjctj/PycharmProjects/myRobotWebsite/PlanetSkyline.png"
+file_input.send_keys(file_path)
+
+# Submit the form
+submit_button = browser.find_element(by='xpath', value="//button[@type='submit']")
+submit_button.click()
+
+# Wait for the page to load
+time.sleep(5)
+
+# Close the browser
+browser.quit()
