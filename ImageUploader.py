5ae9329e362c80b4fc4cd6ff20d3747818954f46
Tobiasc14 <Spamalot7200@gmail.com>
2023-03-28 09:39:06 -0400

    I think I fixed it

--- ImageUploader.py
+++ ImageUploader.py
@@ -7,15 +7,15 @@ options.binary_location = '/usr/bin/chromium-browser'
 browser = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver', options=options)
 
 # Go to the website URL
-url = "https://24.218.29.129/robincam/"
+url = "http://24.218.29.129/robincam/"
 browser.get(url)
 
 # Find the file input element and send the image file path to it
 file_input = browser.find_element(by='name', value='picture')
 file_path = "/home/pi/Pictures/Screenshot.png"
-time.sleep(5)
-file_input.send_keys(file_path)
 
+file_input.send_keys(file_path)
+time.sleep(5)
 # Submit the form
 submit_button = browser.find_element(by='xpath', value="//button[@type='submit']")
 submit_button.click()
